59b1ed318dfcf3a75ef33effddc6ddacdb621f6c
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-05-15 23:34:18 +0300

    card photos added.

--- src/views/components/Sections/EntranceSection.js
+++ src/views/components/Sections/EntranceSection.js
@@ -14,6 +14,10 @@ import CardHeader from "../../../components/Card/CardHeader.js";
 import CardBody from "../../../components/Card/CardBody.js";
 import CardFooter from "../../../components/Card/CardFooter.js";
 import dummy from "../../../assets/img/dummy.png"
+import card1 from "../../../assets/img/card1.png"
+import card2 from "../../../assets/img/card2.png"
+import card3 from "../../../assets/img/card3.png"
+
 
 
 const useStyles = makeStyles(styles);
@@ -26,7 +30,7 @@ export default function EntranceSection() {
             <div style={{paddingRight:100, marginTop:50}}>
                 <Card>
                     <div style={{display:"flex"}}>
-                        <img src={dummy} width={500} height={500}/>
+                        <img src={card1} width={500} height={500}/>
                         <div style={{width:"100%", textAlign:"center", margin:"auto", padding:20}}>
                             <h3>BikeSharing uses virtual zones instead of physical bike stations. 
                                 Virtual zones bring easy installation and management features with no cost.</h3>
@@ -37,7 +41,7 @@ export default function EntranceSection() {
             <div style={{paddingLeft:100, marginTop:50}}>
                 <Card>
                     <div style={{display:"flex", direction:"rtl"}}>
-                        <img src={dummy} width={500} height={500}/>
+                        <img src={card2} width={500} height={500}/>
                         <div style={{width:"100%", textAlign:"center", margin:"auto", padding:20}}>
                             <h3>BikeSharing mobile application offers seamless, intuitive and smooth experience to the users on both iOS and Android platforms</h3>
                         </div>
@@ -47,7 +51,7 @@ export default function EntranceSection() {
             <div style={{paddingRight:100, marginTop:50}}>
                 <Card>
                     <div style={{display:"flex"}}>
-                        <img src={dummy} width={500} height={500}/>
+                        <img src={card3} width={500} height={500}/>
                         <div style={{width:"100%", textAlign:"center", margin:"auto", padding:20}}>
                             <h3>BikeSharing collects the data generated during use. Detailed analysis and reports are presented on BikeSharing dashboard by using the collected data.</h3>
                         </div>
